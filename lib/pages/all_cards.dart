ea8a80550b2f0afe6192ab16ec2dde84e0f69816
EnderByEndera <707475564@qq.com>
2019-11-07 19:37:50 +0800

    [feature-11.2] Added "http" title to client's URL, but IP not correct, Still trying to figure it out; Added client connection to AllCardsPage

--- lib/pages/all_cards.dart
+++ lib/pages/all_cards.dart
@@ -37,51 +37,63 @@ class AllCardsPageState extends State<AllCardsPage> {
   Response res;
   Dio dio = initDio();
 
-  Widget _buildList(BuildContext context, int index) => Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Consumer<CardCounter>(
-              builder: (context, counter, child) => Hero(
-                tag: counter.cardList.elementAt(index).cardKey,
-                child: SizedBox(
-                  height: 137.1,
-                  child: GestureDetector(
-                    onTap: () {
-                        var cardInfo = counter.cardList.elementAt(index);
-                        Navigator.of(context).pushNamed("/cardinfo", arguments: {
-                          "cardId": cardInfo.cardId,
-                          "cardType": cardInfo.cardType,
-                          "remark": cardInfo.remark,
-                          "key": cardInfo.cardKey,
-                        });
-                      },
-                    child: Container(
-                      alignment: Alignment.bottomLeft,
-                      padding: EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage("assets/images/anz_card.png"),
-                            fit: BoxFit.fitHeight,
-                          )
-                      ),
-                      child: Text(
-                        "${counter.cardList.elementAt(index).cardType}\n" +
-                            "${counter.cardList.elementAt(index).cardId}",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: "consolas",
-                            fontSize: 28.0),
+  Widget _buildList(BuildContext context, int index) =>
+      FutureBuilder<Response<String>>(
+          future: dio.get("/api/cards"),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              res = snapshot.requireData;
+              if (res.statusCode == 404) {
+                throw Exception("404 Error");
+              }
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Consumer<CardCounter>(
+                    builder: (context, counter, child) => Hero(
+                      tag: counter.cardList.elementAt(index).cardKey,
+                      child: SizedBox(
+                        height: 137.1,
+                        child: GestureDetector(
+                          onTap: () {
+                            var cardInfo = counter.cardList.elementAt(index);
+                            Navigator.of(context)
+                                .pushNamed("/cardinfo", arguments: {
+                              "cardId": cardInfo.cardId,
+                              "cardType": cardInfo.cardType,
+                              "remark": cardInfo.remark,
+                              "key": cardInfo.cardKey,
+                            });
+                          },
+                          child: Container(
+                            alignment: Alignment.bottomLeft,
+                            padding: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                              image: AssetImage("assets/images/anz_card.png"),
+                              fit: BoxFit.fitHeight,
+                            )),
+                            child: Text(
+                              "${counter.cardList.elementAt(index).cardType}\n" +
+                                  "${counter.cardList.elementAt(index).cardId}",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: "consolas",
+                                  fontSize: 28.0),
+                            ),
+                          ),
+                        ),
                       ),
                     ),
-                  )
+                  ),
                 ),
-              ),
-            ),
-          ),
-        ),
-      );
+              );
+            } else {
+              print("CircularProgressIndicator");
+              return CircularProgressIndicator();
+            }
+          });
 
   @override
   Widget build(BuildContext context) {
@@ -157,13 +169,20 @@ class AllCardsPageState extends State<AllCardsPage> {
               ),
               backgroundColor: Colors.white,
             ),
-            Consumer<CardCounter>(
-              builder: (context, counter, child) => SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                _buildList,
-                childCount: counter.cardList.length,
-              )),
-            )
+            Consumer<CardCounter>(builder: (context, counter, child) {
+              try {
+                return SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                  _buildList,
+                  childCount: counter.cardList.length,
+                ));
+              } on Exception catch (e) {
+                print("Go into AlertDialog");
+                return AlertDialog(
+                  title: Text(e.toString()),
+                );
+              }
+            }),
           ],
         ));
   }
