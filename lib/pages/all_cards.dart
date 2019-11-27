b656fb5a20faaae698796ea584e2b8fdc0791085
EnderByEndera <707475564@qq.com>
2019-11-27 10:36:42 +0800

    [feature-11 refactor 1.0] 1. Refactor all UI layout in AllCardsMainPage and AllCardsPage 2. Add NFC Function in AllCardsMainPage, Project can now use NFC to communicate with other NFC devices 3. All the 'Plus' icons will lead to AddCardWithNumberPage 4. All the cards now don't have any pics, this may be updated in the next sprint 5. pubspec.yaml changed, added flutter_nfc_reader lib

--- lib/pages/all_cards.dart
+++ lib/pages/all_cards.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_nfc_reader/flutter_nfc_reader.dart';
 import 'package:membership_card/model/card_count.dart';
 import 'package:membership_card/model/card_model.dart';
 import 'package:membership_card/pages/add_cards_with_camera.dart';
@@ -37,6 +38,8 @@ class AllCardsMainPageState extends State<AllCardsMainPage>
     with SingleTickerProviderStateMixin {
   TabController _tabController;
 
+  var _dots = List<TextSpan>();
+
   @override
   void initState() {
     super.initState();
@@ -54,7 +57,8 @@ class AllCardsMainPageState extends State<AllCardsMainPage>
 
   @override
   Widget build(BuildContext context) {
-    int _cardNumber = Provider.of<CardCounter>(context).cardList.length;
+    var _cardNumber = Provider.of<CardCounter>(context).cardList.length;
+
     Widget _randomColorContainer = Container(
       decoration: BoxDecoration(
           boxShadow: [BoxShadow(offset: Offset.zero, blurRadius: 5.0)],
@@ -87,7 +91,7 @@ class AllCardsMainPageState extends State<AllCardsMainPage>
               ),
             ),
             onTap: () {
-              Navigator.of(context).pushNamed("/addcamera");
+              Navigator.of(context).pushNamed("/addnumber");
             },
           )
         ],
@@ -112,8 +116,116 @@ class AllCardsMainPageState extends State<AllCardsMainPage>
             Container(
               alignment: Alignment.center,
               child: MaterialButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed("/addnumber");
+                onPressed: () async {
+                  showDialog(
+                    context: context,
+                    builder: (_) => Container(
+                      child: Flex(
+                        direction: Axis.vertical,
+                        children: [
+                          SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.5),
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  boxShadow: [BoxShadow(blurRadius: 3.0)],
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  color: Colors.white),
+                              child: Flex(
+                                direction: Axis.vertical,
+                                children: <Widget>[
+                                  SizedBox(height: 20),
+                                  Container(
+                                    alignment: Alignment.topCenter,
+                                    child: Text(
+                                      "Ready to Scan",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 28.0,
+                                        decoration: TextDecoration.none,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 200,
+                                    child: Image(
+                                      image: AssetImage(
+                                          "assets/backgrounds/nfcIllu.jpg"),
+                                    ),
+                                  ),
+                                  Container(
+                                    alignment: Alignment.center,
+                                    child: RichText(
+                                      text: TextSpan(
+                                        text: "Hold your phone near the fire",
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 16.0,
+                                          decoration: TextDecoration.none,
+                                        ),
+                                        children: _dots,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      color: Colors.grey,
+                                    ),
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.9,
+                                    height: 50,
+                                    child: MaterialButton(
+                                        onPressed: () async {
+                                          try {
+                                            Navigator.of(context).pop();
+                                            NfcData response =
+                                                await FlutterNfcReader.stop();
+                                            print(response.content);
+                                          } on Exception {
+                                            Navigator.of(context).pop();
+                                          }
+                                        },
+                                        child: Text(
+                                          "Cancel",
+                                          style: TextStyle(fontSize: 20.0),
+                                        )),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                  try {
+                    NfcData response = await FlutterNfcReader.read();
+                    print(response.content);
+                    Navigator.of(context).popAndPushNamed("/cardinfo");
+                  } on Exception {
+                    Navigator.of(context).pop();
+                    showDialog(
+                        context: context,
+                        builder: (_) => AlertDialog(
+                              title: Text("NFC Not Found"),
+                              content:
+                                  Text("Please open NFC in your phone first"),
+                              actions: <Widget>[
+                                FlatButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text("already done",
+                                      style: TextStyle(
+                                          color:
+                                              Theme.of(context).primaryColor)),
+                                )
+                              ],
+                            ));
+                  }
                 },
                 child: Image(
                   width: MediaQuery.of(context).size.width / 3,
@@ -135,13 +247,13 @@ class AllCardsMainPageState extends State<AllCardsMainPage>
                       TextSpan(
                           text: "\'Punch\' ",
                           style: TextStyle(
-                            color: Color.fromRGBO(251, 107, 0, 70),
+                            color: Theme.of(context).primaryColor,
                           )),
                       TextSpan(text: "button and hold your phone close to "),
                       TextSpan(
                           text: "Punch Reward ",
                           style: TextStyle(
-                            color: Color.fromRGBO(251, 107, 0, 70),
+                            color: Theme.of(context).primaryColor,
                           )),
                       TextSpan(text: "in the "),
                       TextSpan(
@@ -177,7 +289,7 @@ class AllCardsMainPageState extends State<AllCardsMainPage>
                           if (index == 0) {
                             return GestureDetector(
                               onTap: () {
-                                Navigator.of(context).pushNamed("/addcamera");
+                                Navigator.of(context).pushNamed("/addnumber");
                               },
                               child: Container(
                                 padding: EdgeInsets.symmetric(horizontal: 8.0),
@@ -211,9 +323,11 @@ class AllCardsMainPageState extends State<AllCardsMainPage>
                 : Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 40.0),
                     child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                            MaterialPageRoute(builder: (_) => AllCardsPage()));
+                      onVerticalDragEnd: (details) {
+                        if (details.primaryVelocity <= 0) {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (_) => AllCardsPage()));
+                        }
                       },
                       child: Consumer<CardCounter>(
                         builder: (context, counter, child) => Stack(
@@ -331,10 +445,12 @@ class AllCardsPage extends StatefulWidget {
 class _AllCardsPageState extends State<AllCardsPage>
     with SingleTickerProviderStateMixin {
   TabController _tabController;
+  TextEditingController _textEditingController;
 
   @override
   void initState() {
     super.initState();
+//    _textEditingController.addListener(() => null);
     _tabController = TabController(
       length: 2,
       vsync: this,
@@ -343,113 +459,132 @@ class _AllCardsPageState extends State<AllCardsPage>
 
   @override
   void dispose() {
+//    _textEditingController.dispose();
     _tabController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          padding: EdgeInsets.all(4.0),
-          iconSize: 32.0,
-          icon: Icon(
-            Icons.arrow_back,
-            color: Color.fromRGBO(251, 107, 0, 70),
-          ),
-        ),
-        title: Text(
-          "Puntos",
-          style: TextStyle(
-            decoration: TextDecoration.none,
-            fontSize: 32.0,
-            color: Color.fromRGBO(251, 107, 0, 70),
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        actions: <Widget>[
-          GestureDetector(
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Image(
-                image: AssetImage("assets/buttons/btnAdd.png"),
-              ),
-            ),
-            onTap: () {
-              Navigator.of(context).pushNamed("/addcamera");
+      body: CustomScrollView(slivers: <Widget>[
+        SliverAppBar(
+          pinned: true,
+          bottom: AppBar(
+              backgroundColor: Colors.white,
+              elevation: 0,
+              automaticallyImplyLeading: false,
+              leading: null,
+              title: TextField(
+                controller: _textEditingController,
+                decoration: InputDecoration(
+                    hintText: "SEARCH...",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0))),
+              )),
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
             },
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: ListView.builder(itemBuilder: (context, index) {
-          if (index == 0) {
-            return Hero(
-              tag: "first",
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  border: Border.all(color: Colors.yellow, width: 1.5),
-                  color: Colors.yellow,
+            padding: EdgeInsets.all(4.0),
+            iconSize: 32.0,
+            icon: Icon(
+              Icons.arrow_back,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+          title: Text(
+            "Puntos",
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              fontSize: 32.0,
+              color: Theme.of(context).primaryColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: <Widget>[
+            GestureDetector(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Image(
+                  image: AssetImage("assets/buttons/btnAdd.png"),
                 ),
-                constraints: BoxConstraints(minHeight: 150),
               ),
-            );
-          } else if (index == 1) {
-            return Hero(
-              tag: 'second',
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  border: Border.all(color: Colors.blue, width: 1.5),
-                  color: Colors.blue,
-                ),
-                constraints: BoxConstraints(minHeight: 150),
-              ),
-            );
-          } else if (index == 2) {
-            return Hero(
-              tag: 'third',
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  border: Border.all(color: Colors.purple, width: 1.5),
-                  color: Colors.purple,
-                ),
-                constraints: BoxConstraints(minHeight: 150),
-              ),
-            );
-          } else if (index == 3) {
-            return Hero(
-              tag: 'fourth',
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15.0),
-                  border: Border.all(color: Colors.green, width: 1.5),
-                  color: Colors.green,
-                ),
-                constraints: BoxConstraints(minHeight: 150),
-              ),
-            );
-          } else {
-            return Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15.0),
-                border: Border.all(color: Colors.amber, width: 1.5),
-                color: Colors.amber,
-              ),
-              constraints: BoxConstraints(minHeight: 150),
-            );
-          }
-        }),
-      ),
+              onTap: () {
+                Navigator.of(context).pushNamed("/addnumber");
+              },
+            )
+          ],
+        ),
+        SliverPadding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          sliver: SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                if (index == 0) {
+                  return Hero(
+                    tag: "first",
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        border: Border.all(color: Colors.white, width: 3),
+                        color: Colors.yellow,
+                      ),
+                      constraints: BoxConstraints(minHeight: 150),
+                    ),
+                  );
+                } else if (index == 1) {
+                  return Hero(
+                    tag: 'second',
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        border: Border.all(color: Colors.white, width: 3),
+                        color: Colors.blue,
+                      ),
+                      constraints: BoxConstraints(minHeight: 150),
+                    ),
+                  );
+                } else if (index == 2) {
+                  return Hero(
+                    tag: 'third',
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        border: Border.all(color: Colors.white, width: 3),
+                        color: Colors.purple,
+                      ),
+                      constraints: BoxConstraints(minHeight: 150),
+                    ),
+                  );
+                } else if (index == 3) {
+                  return Hero(
+                    tag: 'fourth',
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        border: Border.all(color: Colors.white, width: 3),
+                        color: Colors.green,
+                      ),
+                      constraints: BoxConstraints(minHeight: 150),
+                    ),
+                  );
+                } else {
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      border: Border.all(color: Colors.white, width: 3),
+                      color: Colors.amber,
+                    ),
+                    constraints: BoxConstraints(minHeight: 150),
+                  );
+                }
+              },
+            ),
+          ),
+        ),
+      ]),
       bottomNavigationBar: TabBar(
         controller: _tabController,
         tabs: <Widget>[
