9ac173e1f900e3f50c09f7385c543bc5480608b5
sekuqnoom <1186161809@qq.com>
2020-03-26 09:52:24 +0800

    deliver the CouponImage to couponPage

--- lib/pages/edit_card.dart
+++ lib/pages/edit_card.dart
@@ -75,6 +75,7 @@ class EditCardPageState extends  State<EditCardPage>
             builder: (context, counter, child) => FlatButton(
               onPressed: () {
                if((_formKey.currentState as FormState).validate()) {
+                 //Navigator.pop(context, cardStoreController.value.text);
                  Navigator.pop(context);
                  counter.editCard(args["card"], cardController.value.text,
                      cardStoreController.value.text);
@@ -93,93 +94,93 @@ class EditCardPageState extends  State<EditCardPage>
       ),
 
       body:GestureDetector(
-    behavior: HitTestBehavior.translucent,
-    onTap: () {
+        behavior: HitTestBehavior.translucent,
+        onTap: () {
 
-    FocusScope.of(context).requestFocus(FocusNode());
-    },
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
         child: Form(
-      key: _formKey,
-      autovalidate: true,
+          key: _formKey,
+          autovalidate: true,
 
-         child: Column(
-        children: <Widget>[
-          Text("Enter the store name and the customer number for your card",textAlign: TextAlign.center,maxLines: 1,style:
-          TextStyle(
-              color: Colors.orange
-          ),),
+          child: Column(
+            children: <Widget>[
+              Text("Enter the store name and the customer number for your card",textAlign: TextAlign.center,maxLines: 1,style:
+              TextStyle(
+                  color: Colors.orange
+              ),),
 
-          TextFormField(
-            controller: cardController,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(10.0),
-              icon: Icon(Icons.credit_card),
-              labelText: 'Please edit you cardNumber',
-            ),
-            //limit MAX input=20
-            inputFormatters:[WhitelistingTextInputFormatter(RegExp("[a-zA-Z0-9]")),LengthLimitingTextInputFormatter(20)] ,
-            validator: (value) {// 校验用户名
-              return value.trim().length > 0 ? null : 'Card Number can not be null';
-            },
-            autofocus: false,
-          ),
-
-          TextFormField(
-            controller: cardStoreController,
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(10.0),
-              icon: Icon(Icons.calendar_view_day),
-              labelText: 'Please edit the storeName',
-            ),
-            //limit MAX input=20
-            inputFormatters:[WhitelistingTextInputFormatter(RegExp("[a-zA-Z0-9_\u4e00-\u9fa5]")),LengthLimitingTextInputFormatter(20)] ,
-            validator: (value) {// 校验用户名
-              return value.trim().length > 0 ? null : 'Store Name can not be null';
-            },
-            autofocus: false,
-          ),
-
-
-          Consumer<CardCounter>(
-            builder: (context, counter, child) => FlatButton(
-              child: Text(
-                'Delete Card',
-                style: TextStyle(fontSize: 20.0),
-              ),
-              color: Colors.orange,
-              colorBrightness: Brightness.dark,
-              disabledColor: Colors.grey,
-              disabledTextColor: Colors.grey,
-              padding: new EdgeInsets.only(
-                bottom: 5.0,
-                top: 5.0,
-                left: 20.0,
-                right: 20.0,
-              ),
-              onPressed: (){
-                Navigator.of(context).popUntil(ModalRoute.withName('/allcardspage'));
-                counter.deleteCard(args["card"]);
-              },
-              shape: RoundedRectangleBorder(
-                side: new BorderSide(
-                  width: 2.0,
-                  color: Colors.transparent,
-                  style: BorderStyle.solid,
+              TextFormField(
+                controller: cardController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(10.0),
+                  icon: Icon(Icons.credit_card),
+                  labelText: 'Please edit you cardNumber',
                 ),
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(10.0),
-                  topLeft: Radius.circular(10.0),
-                  bottomLeft: Radius.circular(10.0),
-                  bottomRight: Radius.circular(10.0),
+                //limit MAX input=20
+                inputFormatters:[WhitelistingTextInputFormatter(RegExp("[a-zA-Z0-9]")),LengthLimitingTextInputFormatter(20)] ,
+                validator: (value) {// 校验用户名
+                  return value.trim().length > 0 ? null : 'Card Number can not be null';
+                },
+                autofocus: false,
+              ),
+
+              TextFormField(
+                controller: cardStoreController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(10.0),
+                  icon: Icon(Icons.calendar_view_day),
+                  labelText: 'Please edit the storeName',
+                ),
+                //limit MAX input=20
+                inputFormatters:[WhitelistingTextInputFormatter(RegExp("[a-zA-Z0-9_\u4e00-\u9fa5]")),LengthLimitingTextInputFormatter(20)] ,
+                validator: (value) {// 校验用户名
+                  return value.trim().length > 0 ? null : 'Store Name can not be null';
+                },
+                autofocus: false,
+              ),
+
+
+              Consumer<CardCounter>(
+                builder: (context, counter, child) => FlatButton(
+                  child: Text(
+                    'Delete Card',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  color: Colors.orange,
+                  colorBrightness: Brightness.dark,
+                  disabledColor: Colors.grey,
+                  disabledTextColor: Colors.grey,
+                  padding: new EdgeInsets.only(
+                    bottom: 5.0,
+                    top: 5.0,
+                    left: 20.0,
+                    right: 20.0,
+                  ),
+                  onPressed: (){
+                    Navigator.of(context).popUntil(ModalRoute.withName('/allcardspage'));
+                    counter.deleteCard(args["card"]);
+                  },
+                  shape: RoundedRectangleBorder(
+                    side: new BorderSide(
+                      width: 2.0,
+                      color: Colors.transparent,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(10.0),
+                      topLeft: Radius.circular(10.0),
+                      bottomLeft: Radius.circular(10.0),
+                      bottomRight: Radius.circular(10.0),
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
-    ),
+        ),
       ),
 
 
