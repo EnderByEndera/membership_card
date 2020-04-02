18b3bfaab6bf1967c3381b32a799bb07da0592e2
Yu979 <55450247+Yu979@users.noreply.github.com>
2020-04-02 12:59:03 +0800

    add changePasswordPage and solve the overflow problem of loginPage

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:membership_card/pages/add_cards_with_camera.dart';
 import 'package:membership_card/pages/all_cards.dart';
 import 'package:membership_card/model/card_count.dart';
+import 'package:membership_card/pages/change_password.dart';
 import 'package:membership_card/pages/coupon.dart';
 import 'package:membership_card/pages/edit_card.dart';
 import 'package:membership_card/pages/help.dart';
@@ -84,7 +85,8 @@ class GoWallet extends StatelessWidget {
               return EditCardPage(card.cardId, card.eName);
             },
             "/user": (_) => UserInfoPage(),
-            "/bottomMenu": (_) => BottomMenuPage()
+            "/bottomMenu": (_) => BottomMenuPage(),
+            "/changePasswordPage": (_) =>ChangePasswordPage(),
           },
         ));
   }
