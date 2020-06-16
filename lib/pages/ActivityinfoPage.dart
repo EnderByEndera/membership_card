7cb6bdc83e707ffa276d8f66b4eb1056658d4d39
morningphase <49161035+morningphase@users.noreply.github.com>
2020-06-17 00:20:34 +0800

    Update ActivityinfoPage.dart

--- lib/pages/ActivityinfoPage.dart
+++ lib/pages/ActivityinfoPage.dart
@@ -21,7 +21,6 @@ import 'package:membership_card/network/cookie.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:membership_card/model/activity_model.dart';
 import 'package:membership_card/model/activity_count.dart';
-
 class ActivityinfoPage extends StatefulWidget {
   @override
   ActivityinfoState createState() {
@@ -31,57 +30,47 @@ class ActivityinfoPage extends StatefulWidget {
 
 class ActivityinfoState extends State<ActivityinfoPage> {
   static List _imageUrls = [
-    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1892437343,3193213548&fm=26&gp=0.jpg',
-    'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1700718690,9797066&fm=26&gp=0.jpg',
-    'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2774630792,60701066&fm=26&gp=0.jpg',
-    'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=837235093,3732460301&fm=26&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1892437343,3193213548&fm=26&gp=0.jpg'
   ];
-
-
 
   Response res;
   Dio dio = initDio();
-  var ab=[];
-  var list = ['1'];
+  var list=[];
 
-  getmy(){
-    dioGetActivities(dio).then((res) async {
-      list.clear();
-      print(res.statusCode);
+  @override
+  void initState() {
+    super.initState();
+      dioGetActivity(dio).then((res) async {
+        print(res.statusCode);
+        print(res.data);
+        List<dynamic> js = res.data['activity'];
+        print(js);
+        List<ActivityInfo> mylist = ActivityCounter.fromJson(js).activityList;
+        int i = 0;
+        print(mylist);
+        var b=[];
+        for (; i < mylist.length; i++) {
 
-      print(res.data);
+          ActivityInfo a = mylist[i];
+          b.add(a.description);
 
+        }
+        setState(() {
+          list = b;
+        });
+        print(list);
 
-      List<dynamic> js = res.data['activity'];
-      print(js);
-
-
-
-      List<ActivityInfo> mylist = ActivityCounter.fromJson(js).activityList;
-
-      int i = 0;
-      print(2);
-      print(mylist);
-
-
-      i=0;
-
-
-      for (; i < mylist.length; i++) {
-        ActivityInfo a = mylist[i];
-        list.add(a.description);
+      });
 
 
-      }
-      print(list);
-    });
-      return null;
-  }
+    }
+
+
+
   getList() {
+    Iterable<Widget> listTitles;
 
-    getmy();
-
-      Iterable<Widget> listTitles = list.map((dynamic item) {
+       listTitles = list.map((dynamic item) {
         print(item);
         return new ListTile(
           isThreeLine: true,
@@ -90,8 +79,14 @@ class ActivityinfoState extends State<ActivityinfoPage> {
           title: new Text('卡片类型'),
           subtitle: new Text(item),
           trailing: new Icon(Icons.arrow_right, color: Colors.green),
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+
         );
       });
+
+
       return listTitles.toList();
   }
 
@@ -127,16 +122,19 @@ class ActivityinfoState extends State<ActivityinfoPage> {
       body: ListView(
         children:<Widget>[
           Container(
-              width: MediaQuery.of(context).size.width,
-              height: 200.0,
-              child: _swiper),
+              height: MediaQuery.of(context).size.height * 0.37,
+              alignment: Alignment(0.5, 0),
+              child: Image(
+                image: AssetImage("assets/backgrounds/starbucksBackground.jpg"),
+              )),
+
 
           SizedBox(height: 30,),
-
           new ListView(
             children: this.getList(),
             shrinkWrap: true, // 添加
             physics: NeverScrollableScrollPhysics(),
+
           )
         ],
       ),
@@ -149,14 +147,7 @@ class ActivityinfoState extends State<ActivityinfoPage> {
 
 
 
-
-
-
-
-
-
-
-
+/*
   var _swiper=new Swiper
     (
     itemBuilder: (BuildContext context, int index) {
@@ -176,8 +167,9 @@ class ActivityinfoState extends State<ActivityinfoPage> {
     scrollDirection: Axis.horizontal,
     autoplay: true,
     onTap: (index) => print('点击了第$index个'),
-  );
 
+  );
+*/
 
 
 }
